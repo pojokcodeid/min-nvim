e298c5461a81888bec0461787a8baecbf09205c0
SF7 Repository <pojokcodeid@gmail.com>
2026-09-13 09:02:17 +0700

    enc: update config dashboard always clean

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -9,39 +9,34 @@ local function is_dashboard()
 end
 
 -- 1. Atur UI saat masuk ke buffer (Dashboard atau Netrw)
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "FileType" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "FileType", "VimEnter" }, {
 	group = dashboard_cleaner,
 	callback = function()
-		local ft = vim.bo.filetype
+		vim.schedule(function()
+			local ft = vim.bo.filetype
 
-		if is_dashboard() then
-			-- Setelan bersih total untuk Dashboard
-			vim.opt.laststatus = 0
-			vim.opt_local.number = false
-			vim.opt_local.relativenumber = false
-			vim.opt_local.signcolumn = "no"
-			vim.opt_local.foldcolumn = "0"
-			vim.opt.guicursor = "a:HiddenCursor-blinkwait0"
-		elseif ft == "netrw" then
-			-- Setelan khusus Netrw (Hanya hilangkan numbering, kursor & statusline tetap ada)
-			vim.opt_local.number = false
-			vim.opt_local.relativenumber = false
-			-- Kembalikan kursor ke normal agar bisa bernavigasi di netrw
-			vim.opt.guicursor =
-				"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-			vim.opt.laststatus = 3 -- Tetap munculkan statusline agar tahu posisi folder (bisa diubah ke 0 jika ingin disembunyikan juga)
-		else
-			-- KEMBALIKAN KE SETTINGAN NORMAL UTK FILE BIASA
-			vim.opt.laststatus = 3
-			-- vim.opt_local.number = true
-			-- vim.opt_local.relativenumber = true
-			vim.opt_local.signcolumn = "yes"
-			vim.opt.guicursor =
-				"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
-		end
+			if is_dashboard() then
+				vim.opt.laststatus = 0
+				vim.opt_local.number = false
+				vim.opt_local.relativenumber = false
+				vim.opt_local.signcolumn = "no"
+				vim.opt_local.foldcolumn = "0"
+				vim.opt.guicursor = "a:HiddenCursor-blinkwait0"
+			elseif ft == "netrw" then
+				vim.opt_local.number = false
+				vim.opt_local.relativenumber = false
+				vim.opt.guicursor =
+					"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+				vim.opt.laststatus = 3
+			else
+				vim.opt.laststatus = 3
+				vim.opt_local.signcolumn = "yes"
+				vim.opt.guicursor =
+					"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+			end
+		end)
 	end,
 })
-
 -- 2. MUNCULKAN KURSOR SEBAGAI 'BAR' SAAT MASUK COMMAND MODE
 vim.api.nvim_create_autocmd("CmdlineEnter", {
 	group = dashboard_cleaner,
--- lua/plugins/cmdline.lua
+++ lua/plugins/cmdline.lua
@@ -2,5 +2,5 @@ require("vim._core.ui2").enable({})
 vim.pack.add({ "https://github.com/rachartier/tiny-cmdline.nvim" })
 require("tiny-cmdline").setup({
 	on_reposition = require("tiny-cmdline").adapters.blink,
-	native_types = {},
+	-- native_types = {},
 })
